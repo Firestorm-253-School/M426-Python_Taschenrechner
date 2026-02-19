9ab3739978dd3b48f0151716d28bcbf82e497f9a
GCGgg000 <gcg007@bluewin.ch>
2026-02-19 10:20:44 +0100

    Refactoring tests

--- unit_tests.py
+++ unit_tests.py
@@ -1,7 +1,17 @@
 import unittest
-from main import multiplizieren, dividieren
+from main import addieren, subtrahieren, multiplizieren, dividieren
 
 class test_taschenrechner(unittest.TestCase):
+    def test_addieren(self):
+        self.assertEqual(addieren(2, 3), 5)
+        self.assertEqual(addieren(-1, 1), 0)
+        self.assertEqual(addieren(0, 0), 0)
+
+    def test_subtrahieren(self):
+        self.assertEqual(subtrahieren(5, 3), 2)
+        self.assertEqual(subtrahieren(1, -1), 2)
+        self.assertEqual(subtrahieren(0, 0), 0)
+    
     def test_multiplizieren(self):
         self.assertEqual(multiplizieren(3, 2), 6)
         self.assertEqual(multiplizieren(-1, 1), -1)
@@ -13,7 +23,6 @@ class test_taschenrechner(unittest.TestCase):
         self.assertEqual(dividieren(-4, -2), 2)
         self.assertEqual(dividieren(-3, 0), None)
         self.assertEqual(dividieren(0, 0), None)
-
 
 if __name__ == '__main__':
     unittest.main()
